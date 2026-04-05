5f88ae97e4f84c3cb3c8ceaf23d02e60c7db59f5
Rathissan <158182860+Rathissan@users.noreply.github.com>
2026-04-05 17:55:19 +0200

    Add files via upload

--- Basketduel/main.java
+++ Basketduel/main.java
@@ -1,19 +1,9 @@
-package main;
-
-import javax.swing.SwingUtilities;
 import Controlleur.ControleurMenu;
-import Vue.MenuPrincipale;
-
 
 public class main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(() -> {
-            // Création du contrôleur
-            ControleurMenu controleur = new ControleurMenu();
+        new ControleurMenu();
 
-            // Affichage du menu principal
-            new MenuPrincipale(controleur);
-        });
     }
 }
